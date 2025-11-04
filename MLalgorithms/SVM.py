9f8cad967b253a5682a02d5d2297a9208a792caf
Thomas Napolitano <thomas.napolitano22@gmail.com>
2025-11-04 11:32:45 -0500

    Add SVM Model, find its accuracies, and create a confusion matrix for it

---
+++ MLalgorithms/SVM.py
@@ -0,0 +1,62 @@
+from sklearn import svm
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+airbnbData = pd.read_csv('../finalizedData/finalizedData.csv')
+
+airbnbData_randomized = airbnbData.sample(frac=1, random_state=42)
+airbnbData_randomized = airbnbData_randomized.reset_index(drop=True)
+
+X = airbnbData_randomized.drop(columns="price_category")
+
+y = airbnbData_randomized["price_category"]
+
+##need to convert any non-numerical columns to numerics
+cat_cols = X.select_dtypes(include=['object']).columns.tolist()
+
+
+bool_cols = X.select_dtypes(include=['bool']).columns.tolist()
+if bool_cols:
+    X[bool_cols] = X[bool_cols].astype(int)
+
+
+X_enc = pd.get_dummies(X, columns=cat_cols, drop_first=True)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X_enc, y, test_size=0.20, random_state=12, shuffle=True)
+model = svm.SVC(kernel='linear')
+
+model.fit(X_train, y_train)
+
+modelPrediction = model.predict(X_test)
+
+amountCorrect = np.sum(modelPrediction == y_test)
+print("Out of " + str (len(y_test)) + " tests. SVM correctly predicted " + str(amountCorrect) + " of them.")
+accuracy = np.sum(y_test == modelPrediction) / modelPrediction.size
+print(f"Accuracy of our SVM Model: {accuracy * 100:.2f}%")
+
+majority_class = y_test.value_counts().idxmax()
+baseline_accuracy = y_test.value_counts(normalize=True).max()
+
+print("Majority class:", majority_class)
+print(f"Baseline accuracy: {baseline_accuracy * 100: .2f} %")
+
+
+########################################################################################
+#Graphs/Diagrams
+########################################################################################
+
+#Confusion Matrix
+cm = confusion_matrix(y_test, modelPrediction)
+displaycm = ConfusionMatrixDisplay(cm, display_labels=model.classes_)
+displaycm.plot(cmap="Reds", colorbar=False)
+plt.title("Confusion Matrix of the SVM (Linear) Model")
+plt.tight_layout()
+plt.savefig("../ModelsandDiagrams/SVMConfusionMatrix.png")
+plt.show()
+
+
